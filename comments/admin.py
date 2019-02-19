48d3eb30079fcad4033ee0fc67cad71358b3d2a4
Logos <admin@attacker.club>
2019-02-19 16:48:37 +0800

    评论显示email

--- comments/admin.py
+++ comments/admin.py
@@ -9,7 +9,7 @@ from .models import Comment
 
 # 评论
 class Commentlist(admin.ModelAdmin):
-    list_display = ['name', 'url', 'article', 'created_time']  # 分类
+    list_display = ['name', 'url', 'article','email','created_time']  # 分类
 
 
 admin.site.register(Comment,Commentlist)
